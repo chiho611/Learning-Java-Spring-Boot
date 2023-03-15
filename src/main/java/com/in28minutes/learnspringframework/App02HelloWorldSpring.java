89dfb91779d3b5c636e55da098f4e1d8182207af
chiho <ramtam611@gmail.com>
2023-03-14 18:01:04 -0700

    update step 11

--- src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
+++ src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
@@ -4,17 +4,27 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App02HelloWorldSpring {
 
-	public static void main(String[] args) {
-		// 1 Launch Spring Content
-		
-		var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class); 
-		// 2 Configure the things that we want Spring to manage - HelloWorldConfiguration @Configuration
-		
-		//name - @Bean
-		//3 Retrieving beans managed by Spring
-		System.out.println(context.getBean("name"));
-		
-		System.out.println(context.getBean("age"));
-	}
+    public static void main(String[] args) {
+        // 1 Launch Spring Content
+
+        var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
+        // 2 Configure the things that we want Spring to manage - HelloWorldConfiguration @Configuration
+
+        //name - @Bean
+        //3 Retrieving beans managed by Spring
+        System.out.println(context.getBean("name"));
+
+        System.out.println(context.getBean("age"));
+
+        System.out.println(context.getBean("person"));
+
+        System.out.println(context.getBean("person2MethodCall"));
+
+        System.out.println(context.getBean("address2"));
+
+//        System.out.println(context.getBean(Address.class));
+
+
+    }
 
 }
--- src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
+++ src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
@@ -3,17 +3,53 @@ package com.in28minutes.learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+record Person(String name, int age, Address address) {
+}
+
+record Address(String firstLine, String city) {
+}
+
 @Configuration
 public class HelloWorldConfiguration {
-	
-	@Bean
-	public String name() {
-		return "Ranga";
-	}
-	
-	@Bean
-	public int age() {
-		return 25;
-	}
+
+    @Bean
+    public String name() {
+        return "Ranga";
+    }
+
+    @Bean
+    public int age() {
+        return 15;
+    }
+
+    @Bean
+    public Person person() {
+        var person = new Person("Ravi", 20, new Address("Main Street", "Utrecht"));
+        return person;
+    }
+
+    @Bean
+    public Person person2MethodCall() {
+        var person = new Person(name(), age(), address()); //name // age
+        return person;
+    }
+
+    @Bean
+    public Person person3Parameters(String name, int age, Address address3) {
+        var person = new Person(name, age, address3);
+        return person;
+    }
+
+    @Bean(name = "address2")
+    public Address address() {
+        var address = new Address("Baker Street", "London");
+        return address;
+    }
+
+    @Bean(name = "address3")
+    public Address address3() {
+        var address = new Address("Motinagar", "Hyderabad");
+        return address;
+    }
 
 }
