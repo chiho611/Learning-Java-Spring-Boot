246876027bc54a28f8bbf38cb4cf6b509e9754cf
chiho <ramtam611@gmail.com>
2023-03-20 18:04:13 -0700

    finish section 4 , point 48

--- src/main/java/com/in28minutes/learnspringframework/examples/c1/BusinessCalculationService.java
+++ src/main/java/com/in28minutes/learnspringframework/examples/c1/BusinessCalculationService.java
@@ -1,10 +1,10 @@
 package com.in28minutes.learnspringframework.examples.c1;
 
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 
-@Component
+@Service
 public class BusinessCalculationService {
     private DataService dataService;
 
